858228478530b5b41e7aceea87fae1339949f5cb
Vaintseishi <xanthreign.palmes-25@cpu.edu.ph>
2026-09-06 22:05:44 +0800

    testing nextjs routing

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function About() {
+    return (
+        <div>
+            <h1>This is the about page</h1>
+            <Link href="/">
+            <button>Go back home</button>
+            </Link>
+        </div>
+    )
+}
