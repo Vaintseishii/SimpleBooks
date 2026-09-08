79c76de7592a4773d3d34ed5f0978cea71540124
Vaintseishii <xanthreign.palmes-25@cpu.edu.ph>
2026-09-08 22:19:15 +0800

    sample layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function About() {
     return (
-        <div>
-            <h1>This is the about page</h1>
+        <div className="min-h-screen w-full bg-[#eef0f2] flex flex-col items-center">
+            <h1 className="text-4xl">About me page</h1>
             <Link href="/">
-            <button>Go back home</button>
+            <button className="bg-blue-700 text-white hover:bg-amber-400 hover:text-black rounded">Go back home</button>
             </Link>
         </div>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-
+import Link from "next/link";
+import About from "./about/page";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <h1>SimpleBooks</h1>
-      <button>Pa utang</button>
-      <button>Pila utang ko</button>
-      <button>Ano ang balaydan</button>
+    <div className="min-h-screen w-full bg-[#eef0f2] flex flex-col items-center">
+      <h1 className="font-bold text-[#153243] text-4xl mt-4">SimpleBooks</h1>
+      <img src="https://preview.redd.it/jack-black-finding-some-gold-v0-mvk1ifapzijg1.jpeg?width=1080&crop=smart&auto=webp&s=c99f0b615230c377a21e860aca4b9981342653ab" className="w-400 h-250"/>
+      <Link href={"about/"}><button className="bg-blue-700 text-white hover:bg-amber-400 hover:text-black rounded">Go to about</button></Link>
     </div>
-    
   )
 }
-
-
